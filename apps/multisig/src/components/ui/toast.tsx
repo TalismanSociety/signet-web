6093f915dd93642e5e0eca54dd3fc371a8848a32
chrisling-dev <chrisling.dev@gmail.com>
2024-01-19 18:07:44 +0800

    feat: new landing page

--- apps/multisig/src/components/ui/toast.tsx
+++ apps/multisig/src/components/ui/toast.tsx
@@ -52,7 +52,7 @@ const ToastAction = React.forwardRef<
   <ToastPrimitives.Action
     ref={ref}
     className={cn(
-      'inline-flex h-8 shrink-0 items-center justify-center rounded-md border bg-transparent px-3 text-sm font-medium ring-offset-background transition-colors hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 group-[.destructive]:border-muted/40 group-[.destructive]:hover:border-destructive/30 group-[.destructive]:hover:bg-destructive group-[.destructive]:hover:text-destructive-foreground group-[.destructive]:focus:ring-destructive',
+      'inline-flex shrink-0 items-center justify-center rounded-[8px] border border-gray-200 bg-transparent pb-[4px] pt-[6px] px-[8px] text-[14px] font-medium transition-colors hover:bg-primary hover:border-primary hover:text-gray-800 disabled:pointer-events-none disabled:opacity-50',
       className
     )}
     {...props}
--- apps/multisig/src/layouts/Footer.tsx
+++ apps/multisig/src/layouts/Footer.tsx
@@ -1,4 +1,6 @@
-const Footer = () => {
+import { cn } from '@util/tailwindcss'
+
+const Footer: React.FC<{ darkTalisman?: boolean }> = ({ darkTalisman }) => {
   return (
     <footer className="flex-col md:flex-row flex items-center justify-center md:justify-between gap-[12px] md:gap-[16px] px-[16px] pb-[16px] lg:px-[96px]">
       <div className="flex items-center gap-x-[16px] gap-y-[6px] [&>p]:!text-center [&>a]:text-center [&>p]:!text-[14px] [&>a]:text-[14px] [&>a]:text-offWhite flex-wrap justify-center">
@@ -26,7 +28,10 @@ const Footer = () => {
       </div>
       <div className="group [&>a]:transition-all [&>a]:duration-300 [&>a]:text-[14px] hover:scale-105 transition-all duration-300">
         <a
-          className="bg-clip-text bg-gradient-to-r from-[#ed726d] to-[#ee94f9] group-hover:text-[transparent]"
+          className={cn(
+            darkTalisman ? 'lg:text-gray-900 lg:group-hover:text-gray-950' : 'group-hover:text-[transparent]',
+            'bg-clip-text bg-gradient-to-r from-[#ed726d] to-[#ee94f9] group-hover:text-[transparent]'
+          )}
           href="https://talisman.xyz"
           target="_blank"
           rel="noreferrer"
@@ -37,7 +42,10 @@ const Footer = () => {
           {' ♥️ '}
         </a>
         <a
-          className="bg-clip-text bg-gradient-to-r from-[#918ff8] to-[#cefd9c] group-hover:text-[transparent]"
+          className={cn(
+            darkTalisman ? 'lg:text-gray-800 lg:group-hover:text-gray-950' : 'group-hover:text-[transparent]',
+            'bg-clip-text bg-gradient-to-r from-[#918ff8] to-[#cefd9c] group-hover:text-[transparent]'
+          )}
           href="https://talisman.xyz"
           target="_blank"
           rel="noreferrer"
