4cc6539ddc4aa1de30df612a3998761f09a53df3
chrisling-dev <chrisling.dev@gmail.com>
2024-01-22 10:10:15 +0800

    feat: whitelist page ui

--- apps/multisig/src/layouts/Onboarding/Waitlist.tsx
+++ apps/multisig/src/layouts/Onboarding/Waitlist.tsx
@@ -7,14 +7,12 @@ export const Waitlist: React.FC = () => (
     <Header />
     <section className="lg:px-[16px] py-[20vh] h-full flex flex-1">
       <div className="flex flex-col items-center justify-center px-[32px] py-[32px] lg:p-[32px] bg-gray-800 mx-auto rounded-[24px] max-w-[560px] h-max">
-        <h1 className="text-[28px] lg:text-[32px] font-bold text-center">You're on waitlist!</h1>
+        <h1 className="text-[24px] lg:text-[32px] font-bold text-center">Account not whitelisted</h1>
         <p className="text-center my-[16px]">
-          You will be able to use Signet once we accept your address into the address.
+          Sign up to get early access to Signet, we will get in touch when your account has been whitelisted.
         </p>
-        <h4 className="text-offWhite font-bold text-[16px] text-center mt-[32px]">Want to move up the waitlist?</h4>
-        <p className="text-center mb-[32px]">Skip the queue by filling a reach out form.</p>
         <Button asLink to="https://talisman.xyz/signet" target="_blank">
-          Skip Waitlist
+          Get Early Access
         </Button>
       </div>
     </section>
