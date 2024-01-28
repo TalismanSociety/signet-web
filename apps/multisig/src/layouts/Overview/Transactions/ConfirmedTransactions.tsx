517c1b42adc6fcda265d56186b2f3eddc6c37194
chrisling-dev <chrisling.dev@gmail.com>
2024-01-28 20:50:04 +0800

    feat: filter indexed transactions

--- apps/multisig/src/layouts/Overview/Transactions/ConfirmedTransactions.tsx
+++ apps/multisig/src/layouts/Overview/Transactions/ConfirmedTransactions.tsx
@@ -1,6 +1,7 @@
 import { unknownConfirmedTransactionsState, useConfirmedTransactions } from '@domains/tx-history'
 import { useRecoilValue } from 'recoil'
 import { TransactionsList } from './TransactionsList'
+import { useMemo } from 'react'
 
 type Props = {
   value: string
@@ -10,12 +11,9 @@ export const ConfirmedTransactions: React.FC<Props> = ({ value }) => {
   const { transactions, loading } = useConfirmedTransactions()
   const unknownConfirmedTransactions = useRecoilValue(unknownConfirmedTransactionsState)
 
-  return (
-    <TransactionsList
-      value={value}
-      loading={loading}
-      transactions={transactions}
-      indexing={unknownConfirmedTransactions.length}
-    />
-  )
+  const realUnknown = useMemo(() => {
+    return unknownConfirmedTransactions.filter(ut => !transactions.find(t => t.executedAt && ut === t.id))
+  }, [transactions, unknownConfirmedTransactions])
+
+  return <TransactionsList value={value} loading={loading} transactions={transactions} indexing={realUnknown.length} />
 }
