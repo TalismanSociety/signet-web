c7d7e65f7b6208fb3118144f952471b8a4aab0f1
Will Urban <will.urban.dev@gmail.com>
2024-05-10 15:38:04 +0700

    refactor: simplified Standard Vote options state updates, so it does not intefere with other voting option states

--- apps/multisig/src/layouts/NewTransaction/Vote/ConvictionsDropdown.tsx
+++ apps/multisig/src/layouts/NewTransaction/Vote/ConvictionsDropdown.tsx
@@ -1,9 +1,10 @@
 import { css } from '@emotion/css'
 import { Select } from '@talismn/ui'
+import { VoteDetailsState } from '@domains/referenda'
 
 type Props = {
   conviction: number
-  onChange: (conviction: number) => void
+  setVoteDetails: React.Dispatch<React.SetStateAction<VoteDetailsState>>
 }
 
 const CONVICTIONS = [1, 2, 4, 8, 16, 32].map((lock, index): [value: number, duration: number] => [index + 1, lock])
@@ -20,8 +21,17 @@ export function createConvictionsOpts(): { headlineText: string; value: number }
 }
 
 // ref: https://github.com/polkadot-js/apps/blob/master/packages/react-components/src/ConvictionDropdown.tsx
-const ConvictionsDropdown: React.FC<Props> = ({ conviction, onChange }) => {
+const ConvictionsDropdown: React.FC<Props> = ({ conviction, setVoteDetails }) => {
   const options = createConvictionsOpts()
+
+  const handleChange = (value: number) => {
+    setVoteDetails(prev => {
+      const updatedVal = { ...prev }
+      updatedVal.details.Standard!.vote.conviction = value
+      return updatedVal
+    })
+  }
+
   return (
     <Select
       className={css`
@@ -29,7 +39,7 @@ const ConvictionsDropdown: React.FC<Props> = ({ conviction, onChange }) => {
           height: 56px;
         }
       `}
-      onChange={onChange}
+      onChange={handleChange}
       value={conviction}
     >
       {options.map(({ headlineText, value }) => (
