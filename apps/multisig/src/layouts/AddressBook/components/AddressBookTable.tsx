b39c2a2458cd9600e2edd008eea909a124872a6e
Will Urban <will.urban.dev@gmail.com>
2024-09-19 18:14:16 +0700

    refactor: handle free plans in AddressBookTable

--- apps/multisig/src/layouts/AddressBook/components/AddressBookTable.tsx
+++ apps/multisig/src/layouts/AddressBook/components/AddressBookTable.tsx
@@ -1,16 +1,12 @@
-import { useMemo, useCallback } from 'react'
+import { useMemo } from 'react'
 import { ContactAddress } from '@domains/offchain-data/address-book/hooks/useGetPaginatedAddressesByOrgId'
 import { PaginatedAddresses } from '@domains/offchain-data/address-book/hooks/useGetPaginatedAddressesByOrgId'
 import { PaginationState, useReactTable, getCoreRowModel, ColumnDef, flexRender } from '@tanstack/react-table'
 import { AccountDetails } from '@components/AddressInput/AccountDetails'
 import { useSelectedMultisig } from '@domains/multisig'
-import useCopied from '@hooks/useCopied'
-import { CircularProgressIndicator, IconButton } from '@talismn/ui'
-import { Copy, Trash } from '@talismn/icons'
-import { useDeleteContact } from '@domains/offchain-data/address-book/address-book'
-import { clsx } from 'clsx'
 import AddressBookPagination from './AddressBookPagination'
-import { useNavigate } from 'react-router-dom'
+import { cn } from '@util/tailwindcss'
+import ActionButtons from './AddressBookActionButtons'
 
 const AddressBookTable = ({
   hideCollaboratorActions,
@@ -18,36 +14,26 @@ const AddressBookTable = ({
   pagination,
   setPagination,
   isCsvImport,
+  isPaidPlan,
   search,
 }: {
   dataQuery: PaginatedAddresses | undefined
   hideCollaboratorActions: boolean
   isCsvImport: boolean
+  isPaidPlan: boolean
   pagination: PaginationState
-
   search: string
   setPagination: React.Dispatch<React.SetStateAction<PaginationState>>
 }) => {
   const [selectedMultisig] = useSelectedMultisig()
-  const { copy } = useCopied()
-  const { deleteContact, deleting } = useDeleteContact()
-
-  const navigate = useNavigate()
 
   const isLastItemInPage =
     (dataQuery?.pageCount ?? -1) > 1 &&
     pagination.pageIndex + 1 === (dataQuery?.pageCount ?? -1) &&
     dataQuery?.rowCount === 1
 
-  const handleAddressDeleteSuccess = useCallback(() => {
-    if (isLastItemInPage) {
-      navigate('#1', { replace: true })
-      setPagination(prev => ({ ...prev, pageIndex: prev.pageIndex - 1 }))
-    }
-  }, [isLastItemInPage, navigate, setPagination])
-
-  const columns = useMemo<ColumnDef<ContactAddress>[]>(
-    () => [
+  const columns = useMemo<ColumnDef<ContactAddress>[]>(() => {
+    const baseColumns: ColumnDef<ContactAddress>[] = [
       {
         header: 'Name',
         accessorKey: 'name',
@@ -68,7 +54,7 @@ const AddressBookTable = ({
         header: 'Address',
         accessorKey: 'address',
         cell: ({ row }) => (
-          <>
+          <div className={cn('flex items-center gap-3', { 'justify-end': !isPaidPlan })}>
             <div className="block lg:hidden truncate">
               {row.original.address.toShortSs58(selectedMultisig.chain, 'sm')}
             </div>
@@ -79,9 +65,22 @@ const AddressBookTable = ({
             <div className="hidden 2xl:block truncate">
               {row.original.address.toShortSs58(selectedMultisig.chain, 'xl')}
             </div>
-          </>
+            {!isPaidPlan && (
+              <ActionButtons
+                row={row}
+                hideCollaboratorActions={hideCollaboratorActions}
+                isCsvImport={isCsvImport}
+                pagination={pagination}
+                isLastItemInPage={isLastItemInPage}
+                setPagination={setPagination}
+              />
+            )}
+          </div>
         ),
       },
+    ]
+
+    const paidColumns: ColumnDef<ContactAddress>[] = [
       {
         header: 'Category',
         accessorKey: 'category.name',
@@ -90,48 +89,37 @@ const AddressBookTable = ({
         header: 'Subcategory',
         accessorKey: 'sub_category.name',
         cell: ({ row }) => {
-          const addressTosS58 = row.original.address.toSs58(selectedMultisig.chain)
           return (
             <div className="flex items-center justify-end">
               <div className="flex items-center truncate gap-3">
                 <div className="truncate">{row.original.sub_category?.name}</div>
-                <div
-                  className={clsx('flex items-center flex-row w-[2rem] text-[#a5a5a5]', {
-                    'w-[5rem]': !hideCollaboratorActions && !isCsvImport,
-                  })}
-                  css={({ color }) => ({
-                    button: { color: color.lightGrey },
-                  })}
-                >
-                  <IconButton onClick={() => copy(addressTosS58, 'Address copied!', addressTosS58)}>
-                    <Copy size={16} />
-                  </IconButton>
-                  {!hideCollaboratorActions && !isCsvImport && (
-                    <IconButton
-                      onClick={() => deleteContact(row.original.id, pagination, handleAddressDeleteSuccess)}
-                      disabled={deleting}
-                    >
-                      {deleting ? <CircularProgressIndicator size={16} /> : <Trash size={16} />}
-                    </IconButton>
-                  )}
-                </div>
+                <ActionButtons
+                  row={row}
+                  hideCollaboratorActions={hideCollaboratorActions}
+                  isCsvImport={isCsvImport}
+                  pagination={pagination}
+                  isLastItemInPage={isLastItemInPage}
+                  setPagination={setPagination}
+                />
               </div>
             </div>
           )
         },
       },
-    ],
-    [
-      copy,
-      deleteContact,
-      deleting,
-      handleAddressDeleteSuccess,
-      hideCollaboratorActions,
-      isCsvImport,
-      pagination,
-      selectedMultisig.chain,
     ]
-  )
+    if (isPaidPlan) {
+      baseColumns.push(...paidColumns)
+    }
+    return baseColumns
+  }, [
+    hideCollaboratorActions,
+    isCsvImport,
+    isLastItemInPage,
+    isPaidPlan,
+    pagination,
+    selectedMultisig.chain,
+    setPagination,
+  ])
 
   const defaultData = useMemo(() => [], [])
 
@@ -161,13 +149,17 @@ const AddressBookTable = ({
   return (
     <div className="pt-4 w-full flex flex-col flex-1">
       <div className="grid gap-[8px]">
-        {/* Column Headers */}
-        <div className="grid grid-cols-[1fr_2fr_1fr_1fr] gap-4 font-semibold px-4">
+        <div
+          className={cn('grid gap-4 font-semibold px-4', {
+            'grid-cols-[1fr_2fr_1fr_1fr]': isPaidPlan,
+            'grid-cols-[1fr_1fr]': !isPaidPlan,
+          })}
+        >
           {table
             .getHeaderGroups()
             .map(headerGroup =>
               headerGroup.headers.map(header => (
-                <div className="p-2 text-left last:text-right last:pr-[50px] text-[14px]">
+                <div className="p-2 text-left last:text-right last:pr-[62px] text-[14px]">
                   {flexRender(header.column.columnDef.header, header.getContext())}
                 </div>
               ))
@@ -178,10 +170,13 @@ const AddressBookTable = ({
         {table.getRowModel().rows.map(row => (
           <div
             key={row.id}
-            className="grid grid-cols-[1fr_2fr_1fr_1fr] gap-4 bg-[#1B1B1B] rounded-[16px] py-4 px-4 items-center"
+            className={cn('grid gap-4 bg-[#1B1B1B] rounded-[16px] py-4 px-4 items-center', {
+              'grid-cols-[1fr_2fr_1fr_1fr]': isPaidPlan,
+              'grid-cols-[1fr_1fr]': !isPaidPlan,
+            })}
           >
             {row.getVisibleCells().map(cell => (
-              <div key={cell.id} className={'p-2 flex-grow truncate'}>
+              <div key={cell.id} className={'p-2 flex-grow truncate last:text-right '}>
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </div>
             ))}
