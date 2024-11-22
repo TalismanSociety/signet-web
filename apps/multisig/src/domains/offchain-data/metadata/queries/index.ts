a3260143165d58b988ca6448c150f5568985d67d
chrisling-dev <chrisling.dev@gmail.com>
2024-11-22 17:00:21 +0800

    fix: missing pending tx

--- apps/multisig/src/domains/offchain-data/metadata/queries/index.ts
+++ apps/multisig/src/domains/offchain-data/metadata/queries/index.ts
@@ -1,11 +1,8 @@
 import { gql } from 'graphql-tag'
 
 export const TXS_METADATA_BY_TIMEPOINTS = gql`
-  query TxMetadataByTimepoints($teamId: uuid!, $chainId: String!, $timepoints: [tx_metadata_bool_exp!]!) {
-    tx_metadata(
-      where: { team_id: { _eq: $teamId }, chain: { _eq: $chainId }, _or: $timepoints }
-      order_by: { created: desc }
-    ) {
+  query TxMetadataByTimepoints($teamId: uuid!, $chainId: String!) {
+    tx_metadata(where: { team_id: { _eq: $teamId }, chain: { _eq: $chainId } }, order_by: { created: desc }) {
       team_id
       timepoint_height
       timepoint_index
